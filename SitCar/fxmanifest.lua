0a0089b8630015a30e10b5f64d31f3b9d57712f5
ZakyLuvya <103451705+ZakyLuvya@users.noreply.github.com>
2022-05-16 20:09:43 +0200

    Update fxmanifest.lua

--- SitCar/fxmanifest.lua
+++ SitCar/fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'bodacious'
 game 'gta5'
 version '1.0.0'
 description 'Simple FiveM command /sc (sitcar)'
-repository ''
+repository 'https://github.com/ZakyLuvya/FiveM-Script/tree/main/SitCar'
 
 client_script {
     'client/client.lua'
