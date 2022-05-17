306e4b2ee7a36ffb0a4ba9c0f4226475c1d08b70
Zaky Luvya <103451705+ZakyLuvya@users.noreply.github.com>
2022-05-17 13:40:21 +0200

    Delete server directory

--- server/sv_fix.lua
+++
@@ -1,7 +0,0 @@
-AddEventHandler('chatMessage', function(source, n, msg)
-	local msg = string.lower(msg)
-	if msg == "/fix" then
-		TriggerClientEvent('zakyboss:fix', source)
-		CancelEvent()
-	end
-end)
