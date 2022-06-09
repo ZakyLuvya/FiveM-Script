8b32d827edcde560ab70caf9a89a8788a7f489c3
Zaky Luvya <103451705+ZakyLuvya@users.noreply.github.com>
2022-06-09 15:03:53 +0200

    Update client.lua

--- SitCar/client/client.lua
+++ SitCar/client/client.lua
@@ -1,24 +1,28 @@
-local nelveh = false
-RegisterCommand('sc', function()
-    local vehicle   = ESX.Game.GetClosestVehicle(GetEntityCoords(PlayerPedId()))
-    local vehCoords, pCoords = GetEntityCoords(vehicle), GetEntityCoords(PlayerPedId())
+ESX = exports.es_extended:getSharedObject()
+
+local seduto = false
+
+RegisterCommand('sitcar', function()
+    local ped = PlayerPedId()
+    local vehicle   = ESX.Game.GetClosestVehicle(GetEntityCoords(ped))
+    local vehCoords, pCoords = GetEntityCoords(vehicle), GetEntityCoords(ped)
     if GetDistanceBetweenCoords(vehCoords, pCoords, true) < 3.0 then
-        if not nelveh then
-            TaskEnterVehicle(PlayerPedId(), vehicle, 2000, 1, 1.0, 1, 0)
+        if not seduto then
+            TaskEnterVehicle(ped, vehicle, 2000, 1, 1.0, 1, 0)
             Citizen.Wait(2000)
-            TaskLeaveVehicle(PlayerPedId(), vehicle, 16)
+            TaskLeaveVehicle(ped, vehicle, 16)
             Citizen.Wait(0)
-            nelveh = true
-            AttachEntityToEntity(PlayerPedId(), vehicle, -1, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, false, false, false, false, 20, true)
+            seduto = true
+            AttachEntityToEntity(ped, vehicle, -1, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, false, false, false, false, 20, true)
             ExecuteCommand('e sit5')
-            exports['okokNotify']:Alert('NOTIFICA', 'Sei entrato nel veicolo!', 5000, 'info')
+            ESX.ShowNotification("Sei entrato nel veicolo")
         else
-            nelveh = false
-            DetachEntity(PlayerPedId())
+            seduto = false
+            DetachEntity(ped)
             ExecuteCommand('e c')
-            exports['okokNotify']:Alert('NOTIFICA', 'Sei uscito dal veicolo!', 5000, 'info')
+            ESX.ShowNotification("Sei uscito dal veicolo")
         end
     else
-        exports['okokNotify']:Alert('NOTIFICA', 'Non ci sono veicoli nelle vicinanze!', 5000, 'info')
+        ESX.ShowNotification("Non ci sono veicoli nelle vicinanze")
     end
 end)
